220f4388fab0c767e80956cb06f6fdc6f79d44d0
shubhankarmandal <123650661+shubhankarmandal@users.noreply.github.com>
2023-05-19 16:32:24 +0530

    Add files via upload

---
+++ assignement 2 (1).py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Q1 
+'''In python, we use symbol for comments to understand the code .Things written after "#" comes under comment
+there are two types of comments'''
+
+#- single line comment = we use # to comment one line or single line .example
+a="shubhankar"  # assigning a string into a variable a
+
+
+#- multiple line comment = we use """ to comment multiple lines. example-
+
+a="shubhankar"  '''hi my name is shubhankar
+   and this is a multiple line comment'''
+
+
+# In[2]:
+
+
+#Q2
+'''Variable is a container through which we assign values like-integer,float,string,boolean,complex
+   
+   To declare and assign variables we take a value or name and use "=" opreator 
+   example-
+   variable = value '''
+
+a = 19     #so here we use a variables and we assign a value as 19
+
+b = "yash" #so here we use b variables and we assign a value as "yash" 
+
+
+# In[3]:
+
+
+#Q3
+# For converting one data type to another we used to take a variable suppose 
+
+num = 59
+
+# And we have to convert it into floating value. so used to write a code -
+
+print(float(num))
+
+# Similarly, we use can also do this with other data types.
+
+
+# In[4]:
+
+
+#Q4
+
+
+# In[5]:
+
+
+#Q5
+
+my_list=[1,2,3,4,5]
+
+my_list[1:3]
+
+
+# In[6]:
+
+
+#Q6
+# For complex number in python we used write a code
+
+complex_num = 5+8j
+
+print(complex_num)
+
+type(complex_num)
+
+# we can only use j for the complex number 
+
+
+# In[7]:
+
+
+#Q7
+
+age = 25 # the variable name age and we assign the value 25
+
+age
+
+
+# In[8]:
+
+
+#Q8
+
+price = 9.99  # the variable name price and we assign the value 9.99
+
+type(price) # for showing data type we used to write
+
+
+# In[9]:
+
+
+#Q9
+name = "Shubhankar Mandal" # the variable named name and we assign the name as 'Shubhankar Mandal'
+
+print(name) # for print name it we used to write
+
+
+# In[10]:
+
+
+#Q10
+
+variable = "Hello, World!" # the variable name variable and we assign the string 'Hello, World!'
+
+variable [-6:-1] # for extracting the World from the variable we write
+
+
+# In[11]:
+
+
+#Q11
+
+# the variable name is_student and we assign the boolean True
+is_student = True # it assumes that you are currently a student
+
+# the variable name is_student and we assign the boolean False
+is_student = False # it assumes that you are currently not a student
+
+
+# In[ ]:
+
+
+
+
